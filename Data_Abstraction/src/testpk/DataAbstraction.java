1d2c977d784d6ca1225de5f4e1cb391bfac6cb2e
aqwa <aqwa@DESKTOP-DS305J5>
2022-04-20 23:56:48 +0530

    Fourth commit

---
+++ Data_Abstraction/src/testpk/DataAbstraction.java
@@ -0,0 +1,19 @@
+package testpk;
+
+abstract class atm {
+	abstract void withdraw();
+}
+
+public class DataAbstraction extends atm
+{
+    void withdraw() {
+    	System.out.println("Withdraw Sucessfully");
+    }
+	public static void main(String[] args) 
+	{
+		DataAbstraction obj1 = new DataAbstraction();
+		obj1.withdraw();
+
+	}
+
+}
