09c2f9d5b673177741af7b63ff14119aad6a7e01
aqwa <aqwa@DESKTOP-DS305J5>
2022-05-09 12:25:33 +0530

    Eighteen commit

---
+++ Two_Array/src/demo/TwoArray_Example.java
@@ -0,0 +1,18 @@
+package demo;
+
+public class TwoArray_Example {
+
+	public static void main(String[] args) 
+	{
+		int[][] a = {{10,20},{30,40},{5,60}};
+		System.out.println("Two dimensional Array::");
+		for(int i=0;i<3;i++) 
+		{
+			for(int j=0;j<2;j++) 
+			{
+				System.out.println(a[i][j]);
+			}
+		}
+	}
+}	
+
---
+++ Two_Array/src/module-info.java
@@ -0,0 +1,2 @@
+module Two_Array {
+}
