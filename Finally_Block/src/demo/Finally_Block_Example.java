625bfd90c65272fab7e45724484a907cd23589dc
aqwa <aqwa@DESKTOP-DS305J5>
2022-05-16 11:22:08 +0530

    Twenty two commit

---
+++ Finally_Block/src/demo/Finally_Block_Example.java
@@ -0,0 +1,18 @@
+package demo;
+
+public class Finally_Block_Example {
+
+	public static void main(String[] args) {
+		try {
+			int data = 25/5;
+			System.out.println(data);
+		}
+		catch(NullPointerException e) {
+			System.out.println(e);
+		}
+		finally {
+			System.out.println("Finally block is always excueted");
+		}
+	}
+
+}
