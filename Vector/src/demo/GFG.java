ecd0b67dd46975f4dd8dc83f470abde6282e2daf
aqwa <aqwa@DESKTOP-DS305J5>
2022-05-23 11:27:32 +0530

    Thirty commit

---
+++ Vector/src/demo/GFG.java
@@ -0,0 +1,21 @@
+package demo;
+
+import java.util.*;
+class GFG {
+
+	public static void main(String[] args) {
+		Vector<Integer> v = new Vector<Integer>();
+		for(int i=1;i<=5;i++)
+			v.add(i);
+		System.out.println(v);
+		
+		v.remove(3);
+		System.out.println(v);
+		
+		for(int i=0;i<v.size();i++)
+			System.out.println(v.get(i) + " ");
+
+
+	}
+
+}
