56a6d7f8eafde24cc0c0a25456bd0f5de8e349a8
aqwa <aqwa@DESKTOP-DS305J5>
2022-06-10 16:41:05 +0530

    Thirty Nine commit

---
+++ testjunit/src/testjunit5/fp1.java
@@ -0,0 +1,18 @@
+package testjunit5;
+
+import org.junit.jupiter.api.Test;
+
+public class fp1 {
+	@Test
+	void disp() {
+		System.out.println("Hi");
+	}
+	@Test
+	void disp2() {
+		System.out.println("Hello");
+	}
+	@Test
+	void disp3() {
+		System.out.println("Kimaya");
+	}
+}
